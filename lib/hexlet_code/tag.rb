0cf3c13136b3446cebd99faf2624ab3c8e6f417f
Rinat Khabibullin <rinat.khabibullin@dualbootpartners.com>
2023-04-11 23:29:35 +0300

    added module for tag building

---
+++ lib/hexlet_code/tag.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module HexletCode
+  class Tag
+    VOID_ELEMENTS_NAMES = ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param",
+                           "source", "track", "wbr>"].freeze
+
+    attr_reader :name, :attributes, :body
+
+    def self.build(name, attributes = {}, &block)
+      new(name, attributes, &block).build
+    end
+
+    def initialize(name, attributes, &block)
+      @name = name
+      @attributes = attributes
+      @body = block.nil? ? "" : block.call
+    end
+
+    def build
+      VOID_ELEMENTS_NAMES.include?(name) ? void_element : paired_element
+    end
+
+    private
+
+    def void_element
+      @void_element ||= %(<#{name}#{stringify_attributes}>)
+    end
+
+    def paired_element
+      @paired_element ||= %(<#{name}#{stringify_attributes}>#{body}</#{name}>)
+    end
+
+    def stringify_attributes
+      @stringify_attributes ||= attributes.to_a.map { |name, value| %( #{name}="#{value}") }.join
+    end
+  end
+end
