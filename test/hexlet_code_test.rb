04cec2edb25a03b7b27d92d1e6e2dabe8fd97f92
Rinat Khabibullin <rinat.khabibullin@dualbootpartners.com>
2023-04-12 01:25:00 +0300

    linter fixes

--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -1,12 +1,14 @@
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "minitest/autorun"
-require 'hexlet_code'
+require "hexlet_code"
 
 class HexletCodeTest < Minitest::Test
   User = Struct.new(:name, :job, keyword_init: true)
 
   def setup
-    @user = User.new name: 'rob'
+    @user = User.new name: "rob"
   end
 
   def test_form_for_without_url
@@ -16,7 +18,7 @@ class HexletCodeTest < Minitest::Test
   end
 
   def test_form_for_with_url
-    form = HexletCode.form_for @user, url: '/users' do |f|
+    form = HexletCode.form_for @user, url: "/users" do |f|
     end
     assert_equal %(<form action="/users" method="post"></form>), form
   end
