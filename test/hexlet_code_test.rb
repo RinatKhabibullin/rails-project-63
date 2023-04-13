e6e7b44c88b5c57d65523bad60bcb00c11fc912e
Rinat Khabibullin <rinat.khabibullin@dualbootpartners.com>
2023-04-13 13:56:19 +0300

    refactor tests

--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -11,53 +11,37 @@ class HexletCodeTest < Minitest::Test
     @user = User.new name: "rob", job: "hexlet", gender: "m"
   end
 
-  def test_form_for_without_url
-    form = HexletCode.form_for @user do |f|
-    end
-
-    html_fixture = File.read(File.expand_path("fixtures/test_form_for_without_url.html", __dir__))
-    assert_equal html_fixture, form
-  end
-
-  def test_form_for_with_url
+  def test_form_for_with_action
     form = HexletCode.form_for @user, url: "/users" do |f|
-    end
-
-    html_fixture = File.read(File.expand_path("fixtures/test_form_for_with_url.html", __dir__))
-    assert_equal html_fixture, form
-  end
-
-  def test_form_for_with_input
-    form = HexletCode.form_for @user, url: "#" do |f|
       f.input :name, class: "user-input"
       f.input :job
-      f.submit
-    end
-
-    html_fixture = File.read(File.expand_path("fixtures/test_form_for_with_input.html", __dir__))
-    assert_equal html_fixture, form
-  end
-
-  def test_form_for_with_textarea
-    form = HexletCode.form_for @user, url: "#" do |f|
       f.input :job, as: :text, rows: 50, cols: 50
+      f.input :job, as: :text
+      f.submit
       f.submit "Wow"
     end
 
-    html_fixture = File.read(File.expand_path("fixtures/test_form_for_with_textarea.html", __dir__))
-    assert_equal html_fixture, form
+    html_fixture = File.read(File.expand_path("fixtures/form_with_action.html", __dir__))
+
+    assert_equal form, html_fixture
   end
 
-  def test_form_for_with_no_attributes_textarea
+  def test_form_for_without_action
     form = HexletCode.form_for @user do |f|
+      f.input :name, class: "user-input"
+      f.input :job
+      f.input :job, as: :text, rows: 50, cols: 50
       f.input :job, as: :text
+      f.submit
+      f.submit "Wow"
     end
 
-    html_fixture = File.read(File.expand_path("fixtures/test_form_for_with_no_attributes_textarea.html", __dir__))
-    assert_equal html_fixture, form
+    html_fixture = File.read(File.expand_path("fixtures/form_without_action.html", __dir__))
+
+    assert_equal form, html_fixture
   end
 
-  def test_unexisted_field
+  def test_form_for_with_unexisted_field
     assert_raises NoMethodError do
       HexletCode.form_for @user, url: "/users" do |f|
         f.input :name
